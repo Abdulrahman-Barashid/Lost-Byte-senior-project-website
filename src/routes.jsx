1ee2c8630ba31efbec43591864e50c88d95dbd18
Abdulrahman Barashid <d7oomuni@users.noreply.github.com>
2026-05-08 20:42:42 +0300

    Fixing routing conflict

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter } from "react-router";
 import { Layout }       from "./components/layout/Layout";
 import { HomePage }     from "./components/pages/HomePage";
 import { AboutPage }    from "./components/pages/AboutPage";
@@ -7,7 +7,7 @@ import { GalleryPage }  from "./components/pages/GalleryPage";
 import { DownloadPage } from "./components/pages/DownloadPage";
 import { SupportPage }  from "./components/pages/SupportPage";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     Component: Layout,
