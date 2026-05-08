c6f93db9ce6133a04da75c7ed75b728d102543ff
Abdulrahman Barashid <d7oomuni@users.noreply.github.com>
2026-05-08 19:51:12 +0300

    Fix routing for github-pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import emailjs from "@emailjs/browser";
 import "./index.css";
 import App from "./App";
 
-emailjs.init("hvTeVwZZzVjYfZGm5"); // initialize EmailJS publicKey
+emailjs.init("hvTeVwZZzVjYfZGm5");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 );
